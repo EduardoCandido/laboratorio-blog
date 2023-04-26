ff1819ffcd81691d8c03928177818e390281ef12
Eduardo Candido <38570289+EduardoCandido@users.noreply.github.com>
2023-04-26 17:59:04 -0300

    first commit

---
+++ spring-injecao-dinamica/src/main/java/com/educandido/springinjecaodinamica/SpringInjecaoDinamicaApplication.java
@@ -0,0 +1,13 @@
+package com.educandido.springinjecaodinamica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInjecaoDinamicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInjecaoDinamicaApplication.class, args);
+	}
+
+}
---
+++ spring-injecao-dinamica/src/test/java/com/educandido/springinjecaodinamica/SpringInjecaoDinamicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandido.springinjecaodinamica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInjecaoDinamicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
