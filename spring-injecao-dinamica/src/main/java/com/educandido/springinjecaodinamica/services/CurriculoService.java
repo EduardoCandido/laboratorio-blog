1ebe7326dbd5610cf778e7eaaf9046e36e9f4674
Eduardo Candido <38570289+EduardoCandido@users.noreply.github.com>
2023-06-05 16:23:48 -0300

    Decidindo implementação com Qualifier

--- spring-injecao-dinamica/src/main/java/com/educandido/springinjecaodinamica/services/CurriculoService.java
+++ spring-injecao-dinamica/src/main/java/com/educandido/springinjecaodinamica/services/CurriculoService.java
@@ -2,11 +2,13 @@ package com.educandido.springinjecaodinamica.services;
 
 import com.educandido.springinjecaodinamica.interfaces.GeraCurriculo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurriculoService {
 
+    @Qualifier("GeraCurriculoPdfService")
     @Autowired
     GeraCurriculo geraCurriculo;
 }
